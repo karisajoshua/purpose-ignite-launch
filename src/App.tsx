56482f90d768be47e2b83647a2cd1af4ca11ca61
Joshua Karisa <55674650+karisajoshua@users.noreply.github.com>
2026-09-15 03:04:02 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,80 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import AboutPage from "./pages/AboutPage.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import GalleryPage from "./pages/GalleryPage.tsx";
-import ProgrammesPage from "./pages/ProgrammesPage.tsx";
-import MentorshipPage from "./pages/MentorshipPage.tsx";
-import ServicesPage from "./pages/ServicesPage.tsx";
-import BlogPage from "./pages/BlogPage.tsx";
-import BlogDetailPage from "./pages/BlogDetailPage.tsx";
-import ContactPage from "./pages/ContactPage.tsx";
-import ProgrammeDetailPage from "./pages/ProgrammeDetailPage.tsx";
-import AdminLoginPage from "./pages/AdminLoginPage.tsx";
-import AdminDashboardPage from "./pages/AdminDashboardPage.tsx";
-import UserDashboardPage from "./pages/UserDashboardPage.tsx";
-import ScrollToTop from "./components/ScrollToTop.tsx";
-import BackToTop from "./components/BackToTop.tsx";
+
+import ScrollToTop from "./components/ScrollToTop";
+import BackToTop from "./components/BackToTop";
+
+import Index from "./pages/Index";
+import AboutPage from "./pages/AboutPage";
+import GalleryPage from "./pages/GalleryPage";
+import ProgrammesPage from "./pages/ProgrammesPage";
+import ProgrammeDetailPage from "./pages/ProgrammeDetailPage";
+import MentorshipPage from "./pages/MentorshipPage";
+import ServicesPage from "./pages/ServicesPage";
+import BlogPage from "./pages/BlogPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
+import ContactPage from "./pages/ContactPage";
+import AdminLoginPage from "./pages/AdminLoginPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import UserDashboardPage from "./pages/UserDashboardPage";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => ( <QueryClientProvider client={queryClient}> <TooltipProvider> <Toaster /> <Sonner />
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
 
-```
-  <BrowserRouter>
-    <ScrollToTop />
-    <BackToTop />
+        <BrowserRouter>
+          <ScrollToTop />
+          <BackToTop />
 
-    <Routes>
-      <Route path="/" element={<Index />} />
+          <Routes>
+            <Route path="/" element={<Index />} />
 
-      <Route path="/about" element={<AboutPage />} />
+            <Route path="/about" element={<AboutPage />} />
 
-      <Route path="/gallery" element={<GalleryPage />} />
+            <Route path="/gallery" element={<GalleryPage />} />
 
-      <Route path="/programmes" element={<ProgrammesPage />} />
+            <Route path="/programmes" element={<ProgrammesPage />} />
 
-      <Route
-        path="/programmes/:slug"
-        element={<ProgrammeDetailPage />}
-      />
+            <Route
+              path="/programmes/:slug"
+              element={<ProgrammeDetailPage />}
+            />
 
-      <Route path="/mentorship" element={<MentorshipPage />} />
+            <Route path="/mentorship" element={<MentorshipPage />} />
 
-      <Route path="/services" element={<ServicesPage />} />
+            <Route path="/services" element={<ServicesPage />} />
 
-      <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog" element={<BlogPage />} />
 
-      <Route
-        path="/blog/:slug"
-        element={<BlogDetailPage />}
-      />
+            <Route path="/blog/:slug" element={<BlogDetailPage />} />
 
-      <Route path="/contact" element={<ContactPage />} />
+            <Route path="/contact" element={<ContactPage />} />
 
-      <Route path="/admin" element={<AdminLoginPage />} />
+            <Route path="/admin" element={<AdminLoginPage />} />
 
-      <Route path="/dashboard" element={<UserDashboardPage />} />
+            <Route path="/dashboard" element={<UserDashboardPage />} />
 
-      <Route
-        path="/admin/dashboard"
-        element={<AdminDashboardPage />}
-      />
+            <Route
+              path="/admin/dashboard"
+              element={<AdminDashboardPage />}
+            />
 
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-</TooltipProvider>
-```
-
-  </QueryClientProvider>
-);
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
